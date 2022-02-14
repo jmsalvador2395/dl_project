ba1210ea04af6bf34b8a52e5c37e1bdcc508c854
jmsalvador2395 <jmsalvador2395@gmail.com>
2022-02-14 14:39:16 -0600

    moved gather experience to my personal branch

--- gather_experience.py
+++ gather_experience.py
@@ -1,5 +1,24 @@
 import numpy as np
 import gym
+from ale_py import ALEInterface
+from pynput import keyboard
+
+k=None
+
+def on_press(key):
+    try:
+        print('key {0} pressed'.format(key.vk))
+        k=key.vk
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+
+def on_release(key):
+    
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+    else:
+        k=None
 
 if __name__ == '__main__':
 
@@ -9,15 +28,30 @@ if __name__ == '__main__':
     s_prime=state
     #print('action_space: {}'.format(env.action_space.n))
 
+    print(env.get_keys_to_action())
+    print(env.get_action_meanings())
 
+    action_map=env.get_keys_to_action()
     experience=[]
-    print(env.action_space)
+
+    print(action_map[(None,)])
+
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release)
+    listener.start()
+
+    #ale=ALEInterface()
+    
+
     while not done:
         #probably should delete this line
         action = env.action_space.sample() 
+        #print('action: {}'.format(action))
 
         #take action and collect reward and s'
-        s_prime, r, done, info = env.step(action) 
+        #s_prime, r, done, info = env.step(action) 
+        s_prime, r, done, info = env.step() 
 
         #capture experience
         experience.append((state, action, s_prime))
