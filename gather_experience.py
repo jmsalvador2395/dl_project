9204b9f66a07f30fa52f12db58689a5ea935a428
jmsalvador2395 <jmsalvador2395@gmail.com>
2022-02-14 16:38:07 -0600

    added folder to contain our data

--- gather_experience.py
+++ gather_experience.py
@@ -2,6 +2,7 @@ import numpy as np
 import gym
 from ale_py import ALEInterface
 from pynput import keyboard
+from gym.utils.play import *
 
 k=None
 
@@ -23,7 +24,8 @@ def on_release(key):
 if __name__ == '__main__':
 
     done=False
-    env = gym.make('Breakout-v0', render_mode='human')
+    #env = gym.make('Breakout-v0', render_mode='human')
+    play(env = gym.make('Breakout-v0'))
     state=env.reset()
     s_prime=state
     #print('action_space: {}'.format(env.action_space.n))
@@ -51,7 +53,7 @@ if __name__ == '__main__':
 
         #take action and collect reward and s'
         #s_prime, r, done, info = env.step(action) 
-        s_prime, r, done, info = env.step() 
+        s_prime, r, done, info = env.step(action) 
 
         #capture experience
         experience.append((state, action, s_prime))
