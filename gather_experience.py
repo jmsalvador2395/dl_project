37e040d560a8432453f69c4a3edb176195c2931a
jmsalvador2395 <jmsalvador2395@gmail.com>
2022-02-14 16:40:17 -0600

    use play() function to gather and store data

--- gather_experience.py
+++ gather_experience.py
@@ -1,63 +1,23 @@
+import sys
 import numpy as np
 import gym
-from ale_py import ALEInterface
-from pynput import keyboard
 from gym.utils.play import *
+import pickle
+import datetime
 
-k=None
 
-def on_press(key):
-    try:
-        print('key {0} pressed'.format(key.vk))
-        k=key.vk
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
+data=[]
 
-def on_release(key):
+"""
+used for saving the data after playing.
+action is an integer
+obs_t should be shape (210, 160, 3)
+"""
+def cb_routine(obs_t, obs_tp1, action, rew, done, info):
+    data.append((obs_t, action))
     
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-    else:
-        k=None
 
 if __name__ == '__main__':
-
-    done=False
-    #env = gym.make('Breakout-v0', render_mode='human')
-    play(env = gym.make('Breakout-v0'))
-    state=env.reset()
-    s_prime=state
-    #print('action_space: {}'.format(env.action_space.n))
-
-    print(env.get_keys_to_action())
-    print(env.get_action_meanings())
-
-    action_map=env.get_keys_to_action()
-    experience=[]
-
-    print(action_map[(None,)])
-
-    listener = keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release)
-    listener.start()
-
-    #ale=ALEInterface()
-    
-
-    while not done:
-        #probably should delete this line
-        action = env.action_space.sample() 
-        #print('action: {}'.format(action))
-
-        #take action and collect reward and s'
-        #s_prime, r, done, info = env.step(action) 
-        s_prime, r, done, info = env.step(action) 
-
-        #capture experience
-        experience.append((state, action, s_prime))
-
-
-        #set state to s_prime
-        state=s_prime
+    play(env = gym.make('Breakout-v0'), zoom=4, callback=cb_routine)
+    with open('data/demonstrator_{}'.format(datetime.datetime.now()), 'wb') as fh:
+        pickle.dump(data, fh)
