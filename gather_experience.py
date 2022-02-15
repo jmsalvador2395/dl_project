2edeacfdf9a3b143bb59a98841be521854f97989
jmsalvador2395 <jmsalvador2395@gmail.com>
2022-02-14 18:09:35 -0600

    changed file naming scheme. program now creates ./data/ folder if it doesn't exist

--- gather_experience.py
+++ gather_experience.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import numpy as np
 import gym
 from gym.utils.play import *
@@ -13,11 +14,15 @@ used for saving the data after playing.
 action is an integer
 obs_t should be shape (210, 160, 3)
 """
-def cb_routine(obs_t, obs_tp1, action, rew, done, info):
-    data.append((obs_t, action))
+def cb_routine(s, s_prime, a, r, done, info):
+    data.append((s, a))
     
 
 if __name__ == '__main__':
     play(env = gym.make('Breakout-v0'), zoom=4, callback=cb_routine)
-    with open('data/demonstrator_{}'.format(datetime.datetime.now()), 'wb') as fh:
+    time=datetime.datetime.now().strftime("%Y_%m_%d_%H%M%S")
+    fldr='./data/'
+    if not os.path.isdir(fldr):
+        os.mkdir(fldr)
+    with open(fldr+'demonstrator_{}.pickle'.format(time), 'wb') as fh:
         pickle.dump(data, fh)
