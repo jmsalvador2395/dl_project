74153de0d88e4ee8cb045127df056d0ddc6be09f
john <jmsalvador2395@gmail.com>
2022-03-13 14:29:21 -0500

    added plotting at the end and fixed data_loading

--- src/demo_classifier.py
+++ src/demo_classifier.py
@@ -8,35 +8,27 @@ import torchvision.transforms as T
 from utilities  import *
 import numpy as np
 from builtins import *
+import matplotlib.pyplot as plt
 
 class Demo_Classifier:
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    batch_size=4000
+    if torch.cuda.is_available():
+        print('using GPU')
+        device='cuda'
+    else:
+        print('using CPU')
+        device='cpu'
     dtype = torch.float32
     def preprocessing(self):
         #transform = T.Compose([T.ToTensor(),T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
         #data_set = torchvision.datasets.ImageFolder(root='../data', transform=transform)
-        X,y= import_data()
-
-        data_set=[]
-        '''
-        for i in range(len(X)):
-            y1=[0]*4
-            for j in range(0,4):
-                if j == y[i]:
-                    y1[j] = 1
-                else:
-                    y1[j] = 0
-                    
-            data_set.append([X[i],np.array(y1)])
-        '''
-        for i in range(len(X)):
-            data_set.append((X[i], y[i]))
+        data_set= import_data()
 
         train_set_size = int(len(data_set) *0.8)
         test_set_size = len(data_set) - train_set_size
         train_set, test_set = data.random_split(data_set, [train_set_size, test_set_size])
-        loader_train = DataLoader(train_set.dataset, batch_size=64)
-        loader_test = DataLoader(test_set.dataset, batch_size=64)
+        loader_train = DataLoader(train_set, batch_size=self.batch_size)
+        loader_test = DataLoader(test_set, batch_size=self.batch_size)
         return loader_train, loader_test
         
     def cnn_model(self):
@@ -65,37 +57,68 @@ class Demo_Classifier:
         )
         return model
 
-    def check_accuracy(self,loader_train,model):
+    def check_accuracy(self,loader_train, loader_test, model):
         num_correct = 0
         num_samples = 0
         model.eval()  
+
         with torch.no_grad():
             for x, y in loader_train:
                 x = x.to(device=self.device, dtype=self.dtype)  # move to device, e.g. GPU
                 y = y.to(device=self.device, dtype=torch.int64)
                 scores = model(x)
                 preds = torch.argmax(scores, dim=1)
-                #_,preds2= torch.argmax(y)
+
                 num_correct += (preds == y).sum()
-                num_samples += preds.size(0)
-            acc = float(num_correct) / num_samples
-            print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
-        
-    def train_part(self,model,loader_train):
-        epochs = 5
-        learning_rate = 0.001
+                num_samples += preds.size()[0]
+            trn_acc = num_correct / num_samples
+            print('Training Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * trn_acc))
+
+
+
+        num_correct = 0
+        num_samples = 0
+        with torch.no_grad():
+            for x, y in loader_test:
+                x = x.to(device=self.device, dtype=self.dtype)  # move to device, e.g. GPU
+                y = y.to(device=self.device, dtype=torch.int64)
+                scores = model(x)
+                preds = torch.argmax(scores, dim=1)
+
+                num_correct += (preds == y).sum()
+                num_samples += preds.size()[0]
+            val_acc = num_correct / num_samples
+            print('Validation: Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * val_acc))
+
+        return trn_acc, val_acc
+
+
+    def train_part(self,model,loader_train, loader_test):
+        epochs = 3
+        learning_rate = 3e-5
         weight_decay = 0.01
         optimizer=torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
         model = model.to(device=self.device) 
+
+        training_accuracy=[]
+        validation_accuracy=[]
+
+        trn_acc, val_acc=self.check_accuracy(loader_train, loader_test, model)
+        training_accuracy.append(trn_acc.cpu())
+        validation_accuracy.append(val_acc.cpu())
+        print()
+        
+
+
         for e in range(epochs):
             for t,(x, y) in enumerate(loader_train):
+
                 model.train()  
                 x = x.to(device=self.device, dtype=self.dtype)
                 y = y.to(device=self.device, dtype=torch.int64)
                 scores = model(x)
                 preds=torch.argmax(scores, dim=1)
                 loss = torch.nn.CrossEntropyLoss()(scores, y)
-
            
                 optimizer.zero_grad()
 
@@ -104,9 +127,28 @@ class Demo_Classifier:
                 optimizer.step()
 
                 if t % 100 == 0:
-                    print('Iteration %d, loss = %.4f' % (t, loss.item()))
-                    self.check_accuracy(loader_train, model)
+                    print('Epoch: {0}, Iteration {0}, loss: {0:.4f}'.format(e, t, loss.item()))
+                    trn_acc, val_acc=self.check_accuracy(loader_train, loader_test, model)
+                    training_accuracy.append(trn_acc.cpu())
+                    validation_accuracy.append(val_acc.cpu())
                     print()
+        
+        rng=range(0, 100*len(training_accuracy), 100)
+        fig, ax=plt.subplots()
+
+        plt.ylim(0, 1)
+
+        ax.plot(rng, training_accuracy,'k--', label='training accuracy')
+        ax.plot(rng, validation_accuracy, 'k:', label='validation accuracy')
+        
+        legend=ax.legend(loc='upper right', shadow=True)
+        legend.get_frame().set_facecolor('C0')
+
+        plt.xlabel('Training Iteration')
+        plt.ylabel('Accuracy')
+        plt.title('Behavior Cloning Training')
+
+        plt.show()
                     
         return model
             
@@ -114,9 +156,7 @@ class Demo_Classifier:
         
 loader_train_set,loader_test_set = Demo_Classifier().preprocessing()   
 model = Demo_Classifier().cnn_model()
-model_aftertrain=Demo_Classifier().train_part(model,loader_train_set)
-print('Test Accuracy:')
-Demo_Classifier().check_accuracy(loader_test_set, model_aftertrain)   
+model_aftertrain=Demo_Classifier().train_part(model,loader_train_set, loader_test_set)
     
     
     
