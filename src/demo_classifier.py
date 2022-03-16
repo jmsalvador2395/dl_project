03821d944502f29c287da6bb18fe648be00084bb
john <jmsalvador2395@gmail.com>
2022-03-15 23:13:44 -0500

    cleaned some stuff up

--- src/demo_classifier.py
+++ src/demo_classifier.py
@@ -5,161 +5,161 @@ from torch.utils.data import DataLoader
 import torch.utils.data as data
 
 import torchvision.transforms as T
-from utilities  import *
+from utilities	import *
 import numpy as np
 from builtins import *
 import matplotlib.pyplot as plt
 
+num_classes=4
+
 class Demo_Classifier:
-    batch_size=4000
-    if torch.cuda.is_available():
-        print('using GPU')
-        device='cuda'
-    else:
-        print('using CPU')
-        device='cpu'
-    dtype = torch.float32
-    def preprocessing(self):
-        #transform = T.Compose([T.ToTensor(),T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
-        #data_set = torchvision.datasets.ImageFolder(root='../data', transform=transform)
-        data_set= import_data()
+	batch_size=4000
+	if torch.cuda.is_available():
+		print('using GPU')
+		device='cuda'
+	else:
+		print('using CPU')
+		device='cpu'
+	dtype = torch.float32
+	def preprocessing(self):
+		#transform = T.Compose([T.ToTensor(),T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+		#data_set = torchvision.datasets.ImageFolder(root='../data', transform=transform)
+		data_set= import_data()
 
-        train_set_size = int(len(data_set) *0.8)
-        test_set_size = len(data_set) - train_set_size
-        train_set, test_set = data.random_split(data_set, [train_set_size, test_set_size])
-        loader_train = DataLoader(train_set, batch_size=self.batch_size)
-        loader_test = DataLoader(test_set, batch_size=self.batch_size)
-        return loader_train, loader_test
-        
-    def cnn_model(self):
-        layer1 = nn.Sequential(
-        nn.Conv2d(4, 16, kernel_size=8, stride=4),
-        nn.BatchNorm2d(16),
-        nn.ReLU(),
-        nn.MaxPool2d(2)
-        )
+		train_set_size = int(len(data_set) *0.8)
+		test_set_size = len(data_set) - train_set_size
+		train_set, test_set = data.random_split(data_set, [train_set_size, test_set_size])
+		loader_train = DataLoader(train_set, batch_size=self.batch_size)
+		loader_test = DataLoader(test_set, batch_size=self.batch_size)
+		return loader_train, loader_test
+		
+	def cnn_model(self):
+		layer1 = nn.Sequential(
+			nn.Conv2d(4, 16, kernel_size=8, stride=4, padding=(1, 4)),
+			nn.BatchNorm2d(16),
+			nn.ReLU(),
+			nn.MaxPool2d(2)
+		)
 
-        layer2 = nn.Sequential(
-        nn.Conv2d(16, 32, kernel_size=4, stride=2),
-        nn.BatchNorm2d(32),
-        nn.ReLU(),
-        nn.MaxPool2d(2)
-        )
-
-
-        fc = nn.Linear(32*5*4, 4)
-
-        model = nn.Sequential(
-        layer1,
-        layer2,
-        nn.Flatten(),
-        fc
-        )
-        return model
-
-    def check_accuracy(self,loader_train, loader_test, model):
-        num_correct = 0
-        num_samples = 0
-        model.eval()  
-
-        with torch.no_grad():
-            for x, y in loader_train:
-                x = x.to(device=self.device, dtype=self.dtype)  # move to device, e.g. GPU
-                y = y.to(device=self.device, dtype=torch.int64)
-                scores = model(x)
-                preds = torch.argmax(scores, dim=1)
-
-                num_correct += (preds == y).sum()
-                num_samples += preds.size()[0]
-            trn_acc = num_correct / num_samples
-            print('Training Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * trn_acc))
+		layer2 = nn.Sequential(
+			nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=(0, 2)),
+			nn.BatchNorm2d(32),
+			nn.ReLU(),
+			nn.MaxPool2d(2)
+		)
 
 
+		fc = nn.Linear(32*6*5, 4)
 
-        num_correct = 0
-        num_samples = 0
-        with torch.no_grad():
-            for x, y in loader_test:
-                x = x.to(device=self.device, dtype=self.dtype)  # move to device, e.g. GPU
-                y = y.to(device=self.device, dtype=torch.int64)
-                scores = model(x)
-                preds = torch.argmax(scores, dim=1)
+		model = nn.Sequential(
+			layer1,
+			layer2,
+			nn.Flatten(),
+			fc
+		)
+		return model
 
-                num_correct += (preds == y).sum()
-                num_samples += preds.size()[0]
-            val_acc = num_correct / num_samples
-            print('Validation: Got %d / %d correct (%.2f)\n' % (num_correct, num_samples, 100 * val_acc))
+	def check_accuracy(self,loader_train, loader_test, model):
+		num_correct = 0
+		num_samples = 0
+		model.eval()  
 
-        return trn_acc, val_acc
+		with torch.no_grad():
+			for x, y in loader_train:
+				x = x.to(device=self.device, dtype=self.dtype)	# move to device, e.g. GPU
+				y = y.to(device=self.device, dtype=torch.int64)
+				scores = model(x)
+				preds = torch.argmax(scores, dim=1)
 
-
-    def train_part(self,model,loader_train, loader_test):
-        epochs = 3
-        learning_rate = 3e-5
-        weight_decay = 0.01
-        optimizer=torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-        model = model.to(device=self.device) 
-
-        training_accuracy=[]
-        validation_accuracy=[]
-
-        trn_acc, val_acc=self.check_accuracy(loader_train, loader_test, model)
-        training_accuracy.append(trn_acc.cpu())
-        validation_accuracy.append(val_acc.cpu())
-        print()
-        
+				num_correct += (preds == y).sum()
+				num_samples += preds.size()[0]
+			trn_acc = num_correct / num_samples
+			print('Training Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * trn_acc))
 
 
-        for e in range(epochs):
-            for t,(x, y) in enumerate(loader_train):
 
-                model.train()  
-                x = x.to(device=self.device, dtype=self.dtype)
-                y = y.to(device=self.device, dtype=torch.int64)
-                scores = model(x)
-                preds=torch.argmax(scores, dim=1)
-                loss = torch.nn.CrossEntropyLoss()(scores, y)
-           
-                optimizer.zero_grad()
+		num_correct = 0
+		num_samples = 0
+		with torch.no_grad():
+			for x, y in loader_test:
+				x = x.to(device=self.device, dtype=self.dtype)	# move to device, e.g. GPU
+				y = y.to(device=self.device, dtype=torch.int64)
+				scores = model(x)
+				preds = torch.argmax(scores, dim=1)
 
-                loss.backward()
+				num_correct += (preds == y).sum()
+				num_samples += preds.size()[0]
+			val_acc = num_correct / num_samples
+			print('Validation: Got %d / %d correct (%.2f)\n' % (num_correct, num_samples, 100 * val_acc))
 
-                optimizer.step()
+		return trn_acc, val_acc
 
-                if t % 100 == 0:
-                    print('Epoch: {0}, Iteration {0}, loss: {0:.4f}'.format(e+1, t+1, loss.item()))
-                    trn_acc, val_acc=self.check_accuracy(loader_train, loader_test, model)
-                    training_accuracy.append(trn_acc.cpu())
-                    validation_accuracy.append(val_acc.cpu())
-                    print()
-        
-        rng=range(0, 100*len(training_accuracy), 100)
-        fig, ax=plt.subplots()
 
-        plt.ylim(0, 1)
+	def train_part(self,model,loader_train, loader_test):
+		epochs = 8
+		learning_rate = 3e-5
+		weight_decay = 0.01
+		optimizer=torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+		model = model.to(device=self.device) 
 
-        ax.plot(rng, training_accuracy,'k--', label='training accuracy')
-        ax.plot(rng, validation_accuracy, 'k:', label='validation accuracy')
-        
-        legend=ax.legend(loc='upper right', shadow=True)
-        legend.get_frame().set_facecolor('C0')
+		training_accuracy=[]
+		validation_accuracy=[]
 
-        plt.xlabel('Training Iteration')
-        plt.ylabel('Accuracy')
-        plt.title('Behavior Cloning Training')
+		trn_acc, val_acc=self.check_accuracy(loader_train, loader_test, model)
+		training_accuracy.append(trn_acc.cpu())
+		validation_accuracy.append(val_acc.cpu())
+		
 
-        plt.show()
-                    
-        return model
-            
-    
-        
-loader_train_set,loader_test_set = Demo_Classifier().preprocessing()   
-model = Demo_Classifier().cnn_model()
-model_aftertrain=Demo_Classifier().train_part(model,loader_train_set, loader_test_set)
-    
-    
-    
-    
-    
-    
+		for e in range(epochs):
+			for t,(x, y) in enumerate(loader_train):
+
+				model.train()  
+				x = x.to(device=self.device, dtype=self.dtype)
+				y = y.to(device=self.device, dtype=torch.int64)
+
+				scores = model(x)
+				loss = torch.nn.CrossEntropyLoss()(scores, y)
+		   
+				optimizer.zero_grad()
+
+				loss.backward()
+
+				optimizer.step()
+
+				if t % 100 == 0:
+					print('Epoch: {0}, Iteration {1}, loss: {2:.4f}'.format(e+1, t+1, loss.item()))
+					trn_acc, val_acc=self.check_accuracy(loader_train, loader_test, model)
+					training_accuracy.append(trn_acc.cpu())
+					validation_accuracy.append(val_acc.cpu())
+					print()
+		
+		rng=range(0, 100*len(training_accuracy), 100)
+		fig, ax=plt.subplots()
+
+		plt.ylim(0, 1)
+
+		ax.plot(rng, training_accuracy,'k--', label='training accuracy')
+		ax.plot(rng, validation_accuracy, 'k:', label='validation accuracy')
+		
+		legend=ax.legend(loc='upper right', shadow=True)
+		legend.get_frame().set_facecolor('C0')
+
+		plt.xlabel('Training Iteration')
+		plt.ylabel('Accuracy')
+		plt.title('Behavior Cloning Training')
+
+		plt.show()
+					
+		return model
+			
+	
+if __name__ == '__main__':
+	loader_train_set,loader_test_set = Demo_Classifier().preprocessing()   
+	model = Demo_Classifier().cnn_model()
+	model_aftertrain=Demo_Classifier().train_part(model,loader_train_set, loader_test_set)
+	
+	
+	
+	
+	
+	
