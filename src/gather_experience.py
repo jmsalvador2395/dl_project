50993bf38e540623e30baac2a8242f067bdc0c84
jmsalvador2395 <jmsalvador2395@gmail.com>
2022-04-20 12:24:07 -0500

    changed where data is saved

--- src/gather_experience.py
+++ src/gather_experience.py
@@ -18,20 +18,26 @@ RIGHT=ord('p')
 
 #action_map={(None,): 0, (32,): 1, (100,): 2, (97,): 3}
 action_map={(None,): 0, (32,): 1, (RIGHT,): 2, (LEFT,): 3}
+
 if __name__ == '__main__':
+
+	#create data path
+	data_path='../data/'
+	if not os.path.exists(data_path):
+			os.makedirs(data_path)
+
+	data_path+='demonstrations/'
+	if not os.path.exists(data_path):
+			os.makedirs(data_path)
+
 
 	#file parameters
 	collector=data_collector()
-	data_dir='../data/'
 	time=datetime.datetime.now().strftime("%Y_%m_%d_%H%M%S")
-	fname=data_dir+'demonstrator_{}.pickle'.format(time)
+	fname=data_path+'demonstrator_{}.pickle'.format(time)
 
 	#create gym environment and collect data
 	play(env = gym.make('BreakoutDeterministic-v4', obs_type='grayscale'), zoom=4, callback=collector.callback, keys_to_action=action_map)
-
-	#create folder for data
-	if not os.path.isdir(data_dir): 
-		os.mkdir(data_dir)
 
 	#prune and process data
 	collector.convert_noops_breakout()
@@ -41,5 +47,5 @@ if __name__ == '__main__':
 	with open(fname, 'wb') as fh:
 		pickle.dump(collector.dump_data(), fh)
 
-	print('*** data written to {} ***'.format(data_dir))
+	print('*** data written to {} ***'.format(data_path))
 
